29013c8c6152e211501be7c793c36dc666417e99
JQCurv <josh.quittner@curv.ai>
2022-02-18 16:10:53 -0500

    - Printing out all needed data from JSON file

--- WeatherApp/WeatherApp/WeatherGetter.swift
+++ WeatherApp/WeatherApp/WeatherGetter.swift
@@ -17,7 +17,6 @@ class WeatherGetter {
         // This is a pretty simple networking task, so the shared session will do.
         let session = URLSession.shared
         let weatherRequestURL = NSURL(string: "\(openWeatherMapBaseURL)?APPID=\(openWeatherMapAPIKey)&q=\(city)")!
-        print(weatherRequestURL)
 
         // The data task retrieves the data.
         let dataTask = session.dataTask(with: weatherRequestURL as URL) {
@@ -30,9 +29,67 @@ class WeatherGetter {
             else {
                 // Case 2: Success
                 // We got a response from the server!
-                print("Raw data:\n\(data!)\n")
-                let dataString = String(data: data!, encoding: String.Encoding.utf8)
-                print("Human-readable data:\n\(dataString!)")
+//                let dataString = String(data: data!, encoding: String.Encoding.utf8)
+//                print("Human-readable data:\n\(dataString!)")
+                
+                do {
+                    // Try to convert that data into a Swift dictionary
+                    let weatherData = try JSONSerialization.jsonObject(
+                        with: data!,
+                        options: .mutableContainers) as! [String: AnyObject]
+                    
+                    // If we made it to this point, we've successfully converted the
+                    // JSON-formatted weather data into a Swift dictionary.
+
+                    //Print all values to the console
+                    //cordinates
+                    print("\n\nLongitude: \(weatherData["coord"]!["lon"]!!)")
+                    print("Latitude: \(weatherData["coord"]!["lat"]!!)")
+                    
+                    //weather
+                    print("\nWeather ID: \((weatherData["weather"]![0]! as! [String:AnyObject])["id"]!)")
+                    print("Weather main: \((weatherData["weather"]![0]! as! [String:AnyObject])["main"]!)")
+                    print("Weather description: \((weatherData["weather"]![0]! as! [String:AnyObject])["description"]!)")
+                    print("Weather icon: \((weatherData["weather"]![0]! as! [String:AnyObject])["icon"]!)")
+                    
+                    //base
+                    print("\nBase: \(weatherData["base"]!)")
+                    
+                    //main temp data
+                    print("\nTemperature: \(weatherData["main"]!["temp"]!!)")
+                    print("Feels like: \(weatherData["main"]!["feels_like"]!!)")
+                    print("Temp min: \(weatherData["main"]!["temp_min"]!!)")
+                    print("Temp max: \(weatherData["main"]!["temp_max"]!!)")
+                    print("Pressure: \(weatherData["main"]!["pressure"]!!)")
+                    print("Humidity: \(weatherData["main"]!["humidity"]!!)")
+                    
+                    //visibility
+                    print("\nVisibility: \(weatherData["visibility"]!)")
+
+                    //wind
+                    print("\nWind direction: \(weatherData["wind"]!["deg"]!!) degrees")
+                    print("Wind speed: \(weatherData["wind"]!["speed"]!!)")
+
+                    //clouds
+                    print("\nCloud cover: \(weatherData["clouds"]!["all"]!!)")
+                    
+                    //date and time
+                    print("\nDate and time: \(weatherData["dt"]!)")
+
+                    //city
+                    print("\nCity: \(weatherData["name"]!)")
+                    
+                    //system readouts
+                    print("\nCountry: \(weatherData["sys"]!["country"]!!)")
+                    print("Sunrise: \(weatherData["sys"]!["sunrise"]!!)")
+                    print("Sunset: \(weatherData["sys"]!["sunset"]!!)")
+                    print("Timzone: \(weatherData["timezone"]!)")
+                }
+                catch let jsonError as NSError {
+                    // An error occurred while trying to convert the data into a Swift dictionary.
+                    print("JSON error description: \(jsonError.description)")
+                }
+
             }
         }
         
