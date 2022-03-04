a8bddfae4747b87387697919393aee3fceb5ae03
JQCurv <josh.quittner@curv.ai>
2022-03-03 23:15:48 -0500

    - Created Celsius to Fahrenheit conversion func

---
+++ WeatherApp/WeatherApp/Components/ConvertingFunctions.swift
@@ -0,0 +1,12 @@
+//
+//  ConvertingFunctions.swift
+//  WeatherApp
+//
+//  Created by Joshua Quittner on 2022-03-03.
+//
+
+import Foundation
+
+func celciusToFarenheit (tempCel: Double) -> Double {
+    return tempCel*(9/5) + 32
+}
