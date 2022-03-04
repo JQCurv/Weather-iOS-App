c2ec7ea11c9b3e7bc4b336eeee225ef65a85a2c6
JQCurv <josh.quittner@curv.ai>
2022-03-03 23:54:28 -0500

    - Have a basic unix time to date converter

--- WeatherApp/WeatherApp/Components/ConvertingFunctions.swift
+++ WeatherApp/WeatherApp/Components/ConvertingFunctions.swift
@@ -10,3 +10,10 @@ import Foundation
 func celciusToFarenheit (tempCel: Double) -> Double {
     return tempCel*(9/5) + 32
 }
+
+func unixToDate (unixTime: Int64) {
+    let epocTime = TimeInterval(unixTime)
+    
+    let myDate = NSDate(timeIntervalSince1970: epocTime)
+    print("Converted Time \(myDate)")
+}
