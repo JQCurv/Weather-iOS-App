b102c823ca501b6ec8dca0545a3026ea89372e6e
JQCurv <josh.quittner@curv.ai>
2022-04-15 15:46:48 -0400

    Removed old TODO

--- WeatherApp/WeatherApp/Components/WeatherViewComponents/WeatherImage.swift
+++ WeatherApp/WeatherApp/Components/WeatherViewComponents/WeatherImage.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct WeatherImage: View {
-    //TODO: Get weather image to display image based on JSON data
     @Binding var image: String
     @Binding var color: Color
     var size: CGFloat
